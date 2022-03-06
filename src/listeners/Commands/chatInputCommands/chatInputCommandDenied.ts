81111cea795f467f18c0caa0daf4ed8af718c371
EvolutionX <jeyush2005@gmail.com>
2022-03-06 19:17:16 +0530

    feat(cooldown): better messages when in cooldown

--- src/listeners/Commands/chatInputCommands/chatInputCommandDenied.ts
+++ src/listeners/Commands/chatInputCommands/chatInputCommandDenied.ts
@@ -1,22 +1,44 @@
+import { Timestamp } from '#lib/structures';
 import {
     Events,
+    Identifiers,
     Listener,
     UserError,
     type ChatInputCommandDeniedPayload,
 } from '@sapphire/framework';
+import type { CommandInteraction } from 'discord.js';
 
 export class UserListener extends Listener<
     typeof Events.ChatInputCommandDenied
 > {
     public run(
-        { context, message: content }: UserError,
+        { context, message: content, identifier }: UserError,
         { interaction }: ChatInputCommandDeniedPayload
     ) {
         if (Reflect.get(Object(context), 'silent')) return;
-
-        return interaction.reply({
-            content,
-            ephemeral: true,
-        });
+        if (identifier === Identifiers.PreconditionCooldown) {
+            const time = new Timestamp(
+                Date.now() + (context as CooldownContext).remaining
+            );
+            return this.reply(
+                interaction,
+                `You are on cooldown. Try again ${time.getRelativeTime()}`
+            );
+        }
+        return this.reply(interaction, content);
+    }
+    private async reply(interaction: CommandInteraction, content: string) {
+        if (interaction.deferred) {
+            return await interaction.editReply({
+                content,
+            });
+        } else
+            return await interaction.reply({
+                content,
+                ephemeral: true,
+            });
     }
 }
+type CooldownContext = {
+    remaining: number;
+};
