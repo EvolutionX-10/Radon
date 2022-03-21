7aab6e3457f8b50034b9cfeb3e85aac2c142dd71
EvolutionX <jeyush2005@gmail.com>
2022-03-21 12:53:32 +0530

    chore(votehook): added a vote check hook

---
+++ src/listeners/Commands/chatInputCommands/chatInputCommandFinish.ts
@@ -0,0 +1,76 @@
+import { color, isOwner } from '#lib/utility';
+import { ApplyOptions } from '@sapphire/decorators';
+import { Events, Listener } from '@sapphire/framework';
+import axios from 'axios';
+import { CommandInteraction, MessageEmbed } from 'discord.js';
+@ApplyOptions<Listener.Options>({
+    event: Events.ChatInputCommandFinish,
+})
+export class UserListener extends Listener {
+    public async run(interaction: CommandInteraction) {
+        if (isOwner(interaction.user)) return;
+        const isvoted = await this.hasVoted(interaction);
+        if (!isvoted && chance(70)) await this.addVoteMsg(interaction);
+    }
+    private async hasVoted(interaction: CommandInteraction) {
+        const { data }: { data: vote } = await axios({
+            url: `https://top.gg/api/bots/944833303226236989/check?userId=${interaction.user.id}`,
+            method: 'GET',
+            headers: {
+                Authorization: process.env.TOP_BOT_TOKEN as string,
+            },
+        });
+        if (data.voted === 0) return false;
+        else return true;
+    }
+    private async addVoteMsg(interaction: CommandInteraction) {
+        const embed = new MessageEmbed()
+            .setColor(color.General)
+            .setTitle('Thank you for using me')
+            .setDescription(
+                'If you liked the experience, please consider voting for me as it really helps the developer!' +
+                    '\n\n' +
+                    'You can vote by clicking [here](https://top.gg/bot/944833303226236989/vote) for top.gg\n' +
+                    'and [here](https://voidbots.net/bot/944833303226236989/vote) for void bots' +
+                    ' or by clicking the buttons below'
+            )
+            .setTimestamp()
+            .setFooter({
+                text: 'Thank you <3',
+                iconURL: interaction.user.displayAvatarURL({ dynamic: true }),
+            })
+            .setThumbnail(this.container.client.user?.displayAvatarURL() ?? '');
+
+        await interaction.followUp({
+            embeds: [embed],
+            components: [
+                {
+                    type: 'ACTION_ROW',
+                    components: [
+                        {
+                            type: 'BUTTON',
+                            label: 'Vote for me on Top.gg!',
+                            url: `https://top.gg/bot/944833303226236989/vote`,
+                            style: 'LINK',
+                        },
+                        {
+                            type: 'BUTTON',
+                            label: 'Vote for me on Void Bots!',
+                            url: `https://voidbots.net/bot/944833303226236989/vote`,
+                            style: 'LINK',
+                        },
+                    ],
+                },
+            ],
+            ephemeral: chance(70),
+        });
+    }
+}
+
+interface vote {
+    voted: 0 | 1;
+}
+
+function chance(percent: number) {
+    return Math.random() * 100 < percent;
+}
