ad5c3cd08c167e6c964637ac370a29206e18156b
EvolutionX <jeyush2005@gmail.com>
2022-04-04 11:41:25 +0530

    chore(warn): added warn class in GuildSettings

--- src/lib/structures/classes/GuildSettings.ts
+++ src/lib/structures/classes/GuildSettings.ts
@@ -2,14 +2,17 @@ import type { Guild } from 'discord.js';
 import { Blacklist } from './Blacklist';
 import { Modlogs } from './Modlogs';
 import { RolesConfig } from './RolesConf';
+import { Warn } from './Warn';
 
 export class GuildSettings {
     blacklists: Blacklist;
     modlogs: Modlogs;
     roles: RolesConfig;
+    warns: Warn;
     constructor(private readonly guild: Guild) {
         this.blacklists = new Blacklist();
         this.modlogs = new Modlogs(this.guild);
         this.roles = new RolesConfig(this.guild);
+        this.warns = new Warn(this.guild);
     }
 }
