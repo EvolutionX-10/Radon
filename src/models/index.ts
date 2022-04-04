2f8aa9856ea7429ddf890a58665eb1cc99d5affd
EvolutionX <jeyush2005@gmail.com>
2022-04-04 11:24:34 +0530

    feat(warn model): added model of warn

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,4 @@
 export { default as modesDB } from './modes';
 export { default as blacklistDB } from './blacklist';
 export { default as guildSettingsDB } from './guildSettings';
+export { default as warnsDB } from './warns';
---
+++ src/models/warns.ts
@@ -0,0 +1,42 @@
+import { container } from '@sapphire/framework';
+import {
+    buildSchema,
+    modelOptions,
+    prop,
+    Severity,
+} from '@typegoose/typegoose';
+
+class warns {
+    @prop({ required: true })
+    public _id!: string; // ? warn id
+    @prop({ required: true })
+    public reason!: string;
+    @prop({ required: true })
+    public mod!: string;
+    @prop({ required: true })
+    public severity!: number;
+    @prop({ required: true })
+    public date!: Date;
+    @prop({ required: true })
+    public expiration!: Date;
+}
+
+class warnlist {
+    @prop({ required: true })
+    public _id!: string; //? user id
+    @prop({ required: true })
+    public warns!: [warns];
+}
+@modelOptions({
+    schemaOptions: { timestamps: true },
+    options: { allowMixed: Severity.ALLOW },
+})
+class guildWarns {
+    @prop({ required: true })
+    public _id!: string; // ? guild id
+    @prop({ required: true })
+    public warnlist!: [warnlist];
+}
+
+const schema = buildSchema(guildWarns);
+export default container.database.model('guildWarns', schema, 'guildWarns');
