11ab4d3a66fe7755cfb4b4562439af7fce0eeb03
EvolutionX <jeyush2005@gmail.com>
2022-04-08 16:24:50 +0530

    perf(ping): removed ping as global command, only works in radon support now

--- src/commands/General/ping.ts
+++ src/commands/General/ping.ts
@@ -14,8 +14,10 @@ export class UserCommand extends RadonCommand {
 		const content = `Pong! (Roundtrip took: ${Math.round(
 			(msg.editedTimestamp || msg.createdTimestamp) - (message.editedTimestamp || message.createdTimestamp)
 		)}ms. Heartbeat: ${Math.round(this.container.client.ws.ping)}ms.)`;
+
 		return send(message, content);
 	}
+
 	public override async chatInputRun(interaction: CommandInteraction) {
 		const msg = (await interaction.reply({
 			content: `Ping?`,
@@ -23,10 +25,12 @@ export class UserCommand extends RadonCommand {
 			fetchReply: true
 		})) as Message;
 		const { diff, ping } = this.getPing(msg, interaction);
+
 		return interaction.editReply({
 			content: `Pong! (Roundtrip took: ${diff}ms. Heartbeat: ${ping}ms.)`
 		});
 	}
+
 	public override async registerApplicationCommands(registry: RadonCommand.Registry) {
 		registry.registerChatInputCommand(
 			{
@@ -34,11 +38,12 @@ export class UserCommand extends RadonCommand {
 				description: this.description
 			},
 			{
-				guildIds: vars.guildIds,
-				idHints: ['947723983132966934', '951679296689287239']
+				guildIds: vars.radonGuildId, // * Only run in Radon Support *
+				idHints: ['961940451135479888', '951679296689287239']
 			}
 		);
 	}
+
 	private getPing(message: Message, interaction: CommandInteraction) {
 		const diff = (message.editedTimestamp || message.createdTimestamp) - interaction.createdTimestamp;
 		const ping = Math.round(this.container.client.ws.ping);
