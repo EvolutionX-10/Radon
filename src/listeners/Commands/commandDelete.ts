215ae159979e5a528fe459cbe457f70d8a28afa6
EvolutionX <jeyush2005@gmail.com>
2022-04-20 17:30:18 +0530

    perf: remove freeing up of messages

--- src/listeners/Commands/commandDelete.ts
+++ src/listeners/Commands/commandDelete.ts
@@ -2,16 +2,17 @@ import { Events, Listener, ListenerOptions } from '@sapphire/framework';
 import type { Message } from 'discord.js';
 import { get } from '@sapphire/plugin-editable-commands';
 import { ApplyOptions } from '@sapphire/decorators';
-import { mins } from '#lib/utility';
+
 @ApplyOptions<ListenerOptions>({
 	event: Events.MessageDelete
 })
 export class UserListener extends Listener<typeof Events.MessageDelete> {
 	async run(message: Message) {
 		if (!message.guild) return;
+
 		const response = get(message);
 		if (!response) return;
-		if (message.createdTimestamp < Date.now() - mins(30)) return;
-		response.deletable ? await response.delete() : null;
+
+		return response.deletable ? response.delete() : null;
 	}
 }
