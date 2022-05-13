9c15e42567502568d4141502828a86e7361e0506
EvolutionX <jeyush2005@gmail.com>
2022-05-13 23:09:14 +0530

    feat(ban): ban confirmations are now available

--- src/commands/Moderation/ban.ts
+++ src/commands/Moderation/ban.ts
@@ -1,9 +1,9 @@
-import { RadonCommand } from '#lib/structures';
+import { Confirmation, RadonCommand } from '#lib/structures';
 import { PermissionLevels } from '#lib/types';
 import { generateModLogDescription, runAllChecks, sec, severity } from '#lib/utility';
 import { vars } from '#vars';
 import { ApplyOptions } from '@sapphire/decorators';
-import { Constants, GuildMember } from 'discord.js';
+import { Constants, GuildMember, TextChannel } from 'discord.js';
 @ApplyOptions<RadonCommand.Options>({
 	cooldownDelay: sec(10),
 	cooldownLimit: 3,
@@ -14,49 +14,31 @@ import { Constants, GuildMember } from 'discord.js';
 })
 export class UserCommand extends RadonCommand {
 	public override async chatInputRun(interaction: RadonCommand.ChatInputCommandInteraction) {
-		if (!interaction.guild) return;
 		const member = interaction.options.getMember('member') as GuildMember;
 		if (!member)
 			return interaction.reply({
 				content: `${vars.emojis.cross} You must specify a valid member`,
 				ephemeral: true
 			});
-		const reason = interaction.options.getString('reason');
-		const days = interaction.options.getInteger('days');
+		const reason = interaction.options.getString('reason') ?? undefined;
+		const days = interaction.options.getInteger('days') ?? 0;
+
 		const { content: ctn, result } = runAllChecks(interaction.member as GuildMember, member, 'ban');
 		if (!result) return interaction.reply({ content: ctn, ephemeral: true });
-		let content = `${vars.emojis.confirm} ${member} [${member.user.username}] has been banned ${
-			reason ? `for the following reason: ${reason}` : ''
-		}`;
-		await member
-			.send({
-				content: `You have been banned from ${interaction.guild.name}\n${reason ? `Reason: ${reason}` : ''}`
-			})
-			.catch(() => (content += `\n${vars.emojis.cross} Couldn't DM member!`));
-		await member.ban({
-			days: days ?? 0,
-			reason: reason ?? undefined
+
+		const confirm = new Confirmation({
+			content: `Are you sure you want to ban ${member.user.tag}?${reason ? `\nReason: ${reason}` : ''}`,
+			ephemeral: (interaction.channel as TextChannel).visible(),
+			onConfirm: async () => {
+				await this.ban(interaction, member, reason, days);
+			},
+			onCancel: ({ i }) => {
+				return i.editReply({
+					content: `${vars.emojis.cross} Wow careful, stopped the ban hammer at the last moment!`
+				});
+			}
 		});
-		const embed = this.container.utils
-			.embed()
-			._color(severity.ban)
-			._author({
-				name: interaction.user.tag,
-				iconURL: interaction.user.displayAvatarURL({ dynamic: true })
-			});
-		const description = generateModLogDescription({
-			member,
-			action: 'Ban',
-			reason: reason ?? undefined
-		});
-		embed._description(description);
-		if (interaction.guild && (await interaction.guild.settings?.modlogs.modLogs_exist())) {
-			await interaction.guild.settings?.modlogs.sendModLog(embed);
-		}
-		return interaction.reply({
-			content,
-			ephemeral: true
-		});
+		await confirm.run(interaction);
 	}
 
 	public override registerApplicationCommands(registry: RadonCommand.Registry) {
@@ -92,5 +74,43 @@ export class UserCommand extends RadonCommand {
 				idHints: ['947756361876389898', '951679301030387742']
 			}
 		);
+	}
+
+	private async ban(interaction: RadonCommand.ChatInputCommandInteraction, member: GuildMember, reason: string | undefined, days: number) {
+		let content = `${vars.emojis.confirm} ${member} [${member.user.username}] has been banned ${
+			reason ? `for the following reason: ${reason}` : ''
+		}\nhttps://tenor.com/view/11035060`;
+
+		await member
+			.send({
+				content: `You have been banned from ${interaction.guild!.name}\n${reason ? `Reason: ${reason}` : ''}`
+			})
+			.catch(() => (content += `\n${vars.emojis.cross} Couldn't DM member!`));
+		await member.ban({
+			days,
+			reason
+		});
+
+		const embed = this.container.utils
+			.embed()
+			._color(severity.ban)
+			._author({
+				name: interaction.user.tag,
+				iconURL: interaction.user.displayAvatarURL({ dynamic: true })
+			});
+		const description = generateModLogDescription({
+			member,
+			action: 'Ban',
+			reason
+		});
+		embed._description(description);
+
+		if (await interaction.guild!.settings?.modlogs.modLogs_exist()) {
+			await interaction.guild!.settings?.modlogs.sendModLog(embed);
+		}
+
+		return interaction.editReply({
+			content
+		});
 	}
 }
