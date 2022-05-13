fd4f9db6e8d1afad76e5f4738982f1b6de2f25b3
EvolutionX <jeyush2005@gmail.com>
2022-05-13 21:59:15 +0530

    fix: remove any

--- src/lib/utility/functions/pickRandom.ts
+++ src/lib/utility/functions/pickRandom.ts
@@ -1,5 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function pickRandom(array: Array<any>) {
+export function pickRandom<T>(array: Array<T>) {
 	if (!array) throw new Error('No array was provided!');
 	return array[Math.floor(Math.random() * array.length)];
 }
