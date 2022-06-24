1511f10d4d6bba7a3764fd74e152f947e9938338
EvolutionX <jeyush2005@gmail.com>
2022-06-24 15:57:44 +0530

    feat(role): add info subcmd

--- src/commands/Utility/role.ts
+++ src/commands/Utility/role.ts
@@ -1,4 +1,4 @@
-import { RadonCommand } from '#lib/structures';
+import { RadonCommand, Timestamp } from '#lib/structures';
 import { PermissionLevels } from '#lib/types';
 import { vars } from '#vars';
 import { ApplyOptions } from '@sapphire/decorators';
@@ -19,6 +19,8 @@ export class UserCommand extends RadonCommand {
 				return this.add(interaction);
 			case 'remove':
 				return this.remove(interaction);
+			case 'info':
+				return this.list(interaction);
 		}
 	}
 
@@ -77,6 +79,19 @@ export class UserCommand extends RadonCommand {
 								required: false
 							}
 						]
+					},
+					{
+						name: 'info',
+						description: 'Shows information about role',
+						type: Constants.ApplicationCommandOptionTypes.SUB_COMMAND,
+						options: [
+							{
+								name: 'role',
+								description: 'Role to display information about',
+								type: Constants.ApplicationCommandOptionTypes.ROLE,
+								required: true
+							}
+						]
 					}
 				]
 			},
@@ -113,7 +128,8 @@ export class UserCommand extends RadonCommand {
 		await target.roles.add(role, reason).catch((e) => console.log(e.message));
 
 		return interaction.reply({
-			content: `Added ${role} to ${target} successfully!`
+			content: `Added ${role} to ${target} successfully!`,
+			allowedMentions: { parse: [] }
 		});
 	}
 
@@ -143,9 +159,64 @@ export class UserCommand extends RadonCommand {
 		await target.roles.remove(role, reason).catch((e) => console.log(e.message));
 
 		return interaction.reply({
-			content: `Removed ${role} to ${target} successfully!`
+			content: `Removed ${role} to ${target} successfully!`,
+			allowedMentions: { parse: [] }
 		});
+	}
+
+	private list(interaction: RadonCommand.ChatInputCommandInteraction) {
+		const role = interaction.options.getRole('role', true) as Role;
+		const date = new Timestamp(role.createdTimestamp);
+
+		const basic =
+			`- Created At ${date.getShortDateTime()} [${date.getRelativeTime()}]\n` +
+			`- Hex: *\`${role.hexColor}\`*\n` +
+			`- Hoisted: ${role.hoist ? 'Yes' : 'No'}\n` +
+			`- Restricted to Bot: ${role.tags?.botId ? `Yes [<@${role.tags?.botId}>]` : 'No'}\n` +
+			`- Position: ${role.position}\n` +
+			`- Mentionable: ${role.mentionable ? 'Yes' : 'No'}\n` +
+			`- Managed externally: ${role.managed ? 'Yes' : 'No'}`;
+
+		let perms = role.permissions
+			.toArray()
+			.map((e) =>
+				e
+					.split(`_`)
+					.map((i) => i[0] + i.match(/\B(\w+)/)?.[1]?.toLowerCase())
+					.join(` `)
+			)
+			.filter((f) => f.match(/mem|mana|min|men/gim))
+			?.sort();
+		if (perms.includes('Administrator')) perms = ['Administrator'];
+
+		const adv = `- Key Permissions: ${perms.length ? perms.join(' | ') : 'None!'}\n- ID: *\`${role.id}\`*\n- Members: ${role.members.size}`;
+
+		const embed = this.container.utils
+			.embed()
+			._author({
+				name: 'Role Information'
+			})
+			._color(role.color)
+			._timestamp()
+			._title(role.name)
+			._thumbnail(role.iconURL() ?? '')
+			._footer({
+				text: `Requested by ${interaction.user.username}`,
+				iconURL: interaction.user.displayAvatarURL({ dynamic: true })
+			})
+			._fields(
+				{
+					name: 'Basic Info',
+					value: basic
+				},
+				{
+					name: 'Advanced Info',
+					value: adv
+				}
+			);
+
+		return interaction.reply({ embeds: [embed] });
 	}
 }
 
-type SubCommands = 'add' | 'remove';
+type SubCommands = 'add' | 'remove' | 'info';
