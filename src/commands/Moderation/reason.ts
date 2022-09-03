f485ceb16f2bfbc78ea77ca50b01fecf35d4dd53
Evo <85353424+EvolutionX-10@users.noreply.github.com>
2022-09-03 11:38:38 +0530

    feat(reason): use modals! (#267)

--- src/commands/Moderation/reason.ts
+++ src/commands/Moderation/reason.ts
@@ -1,29 +1,33 @@
-import { RadonCommand } from '#lib/structures';
 import { GuildIds } from '#constants';
+import { RadonCommand } from '#lib/structures';
+import { PermissionLevels } from '#lib/types';
 import { ApplyOptions } from '@sapphire/decorators';
-import type { GuildMember, Message, TextChannel } from 'discord.js';
+import { MessageActionRow, Modal, TextChannel, TextInputComponent } from 'discord.js';
 @ApplyOptions<RadonCommand.Options>({
-	description: `Change the reason for the action`
+	description: `Change the reason for the action`,
+	permissionLevel: PermissionLevels.Moderator
 })
 export class UserCommand extends RadonCommand {
 	public override async chatInputRun(interaction: RadonCommand.ChatInputCommandInteraction) {
-		await interaction.deferReply({ ephemeral: true, fetchReply: true });
 		const id = interaction.options.getString('id', true);
-		const reason = interaction.options.getString('reason', true);
 
 		const channelID = await interaction.guild.settings?.modlogs.modLogs_exist();
 		if (!channelID) {
-			return interaction.editReply({
+			return interaction.reply({
 				content: `The modlogs aren't set up for this server.\nPlease inform admins to use \`/setup\``
 			});
 		}
+
 		const channel = (await interaction.guild.channels.fetch(channelID).catch(() => null)) as TextChannel;
+
 		if (!channel) {
-			return interaction.editReply({
+			return interaction.reply({
 				content: `The modlogs channel seems to be deleted.\nPlease inform admins to use \`/setup\``
 			});
 		}
+
 		const message = await channel.messages.fetch(id).catch(() => null);
+
 		if (!message) {
 			return interaction.editReply({
 				content: `The message seems to be deleted or the ID is invalid!`
@@ -35,24 +39,22 @@ export class UserCommand extends RadonCommand {
 				content: `This message isn't from me!`
 			});
 		}
-		const regex = /(\*\*Reason\*\*: )(?:.+)/gim;
-		if (!message.embeds[0].description) return;
-		message.embeds[0].description = message.embeds[0].description?.replace(regex, `$1${reason}`);
-		await message.edit({
-			embeds: message.embeds
-		});
 
-		if (message.embeds[0].description.includes('**Action**: Warn')) {
-			const id = this.getID(message);
+		const modal = new Modal().setCustomId(`@reason/${channelID}/${id}`).setTitle('Edit Reason');
 
-			const member = await interaction.guild.members.fetch(id!.memberID!).catch(() => null);
-			if (member) {
-				await this.updateReason(member, id!.warnID!, reason);
-			}
-		}
-		return interaction.editReply({
-			content: `Successfully updated the reason!`
-		});
+		const reasonInput = new TextInputComponent()
+			.setLabel('Reason')
+			.setCustomId('reason')
+			.setMaxLength(512)
+			.setPlaceholder('Enter the new reason')
+			.setStyle('PARAGRAPH')
+			.setRequired();
+
+		const row = new MessageActionRow<TextInputComponent>().setComponents(reasonInput);
+
+		modal.setComponents(row);
+
+		return interaction.showModal(modal);
 	}
 
 	public override registerApplicationCommands(registry: RadonCommand.Registry) {
@@ -66,41 +68,11 @@ export class UserCommand extends RadonCommand {
 							.setName('id')
 							.setDescription('The ID of the message in the modlogs')
 							.setRequired(true)
-					)
-					.addStringOption((option) =>
-						option //
-							.setName('reason')
-							.setDescription('The updated reason for the action')
-							.setRequired(true)
 					),
 			{
 				guildIds: GuildIds,
 				idHints: ['952460616696741938', '952277309015093288']
 			}
 		);
-	}
-
-	private async updateReason(member: GuildMember, id: string, reason: string) {
-		const warns = await member.guild.settings?.warns.get({ member });
-		if (!warns || !warns?.doc || !warns?.person) return;
-		const warn = warns.person.warns.find((warn) => warn.id === id);
-		if (!warn) return;
-		warn.reason = reason;
-		return member.guild.settings?.warns.update({ member, warns: warns.person.warns });
-	}
-
-	private getID(message: Message) {
-		const embed = message.embeds[0];
-		if (!embed) return;
-		const regexForMember = /`(?<MemberID>\d{17,19})`/gm;
-		const regexForId = /(?:\*\*Warn ID\*\*: `)(?<warnID>.{17})(?:`)/gm;
-		const match = regexForMember.exec(embed.description as string);
-		const match2 = regexForId.exec(embed.description as string);
-
-		if (!match?.length || !match2?.length) return;
-		return {
-			warnID: match2.groups?.warnID,
-			memberID: match.groups?.MemberID
-		};
 	}
 }
