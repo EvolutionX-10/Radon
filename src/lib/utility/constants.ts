9dfc99547a3e78f29fc02f2c89f1f1a3e5735e15
Evo <85353424+EvolutionX-10@users.noreply.github.com>
2022-10-05 08:33:52 +0530

    feat(user): new command /about user (#281)
    ## Other Changes
    - moves stats command embed to stats.ts
    - removes stats from /about me
    - adds new section for messages, #lib/messages
    - updates constants.ts with new emojis
    - minor bug fix in formatter.ts

--- src/lib/utility/constants.ts
+++ src/lib/utility/constants.ts
@@ -1,3 +1,5 @@
+import type { UserFlagsString } from 'discord.js';
+
 process.env.NODE_ENV ??= 'development';
 
 export const enum Emojis {
@@ -7,7 +9,8 @@ export const enum Emojis {
 	Right = '<:arrow_lilright:959363096781148160>',
 	Backward = '<:arrow_left_r:959362559595655179>',
 	Forward = '<:arrow_right_r:959361662064930827>',
-	Stop = '<:radon_stop:959386465807265794>'
+	Stop = '<:radon_stop:959386465807265794>',
+	Owner = '<:owner:1026798797839409173>'
 }
 
 export const enum Color {
@@ -47,3 +50,20 @@ export const Prefixes = process.env.NODE_ENV === 'development' ? ['$', ''] : ['$
 export const RadonGuildId = ['944833697251721246'];
 
 export const TestServerGuildIds = ['953175228899553312', '991194621763919971'];
+
+export const UserFlags: Record<UserFlagsString, string> = {
+	VERIFIED_BOT: '<:vbot:1026794095735226418>',
+	BOT_HTTP_INTERACTIONS: '',
+	BUGHUNTER_LEVEL_1: '<:BadgeBugHunter:1024918366042996797>',
+	BUGHUNTER_LEVEL_2: '<:BadgeBugHunterLvl2:1024918372607082496>',
+	DISCORD_CERTIFIED_MODERATOR: '<:BadgeCertifiedMod:1024918378621706250>',
+	DISCORD_EMPLOYEE: '<:BadgeStaff:1024918411832213525>',
+	EARLY_SUPPORTER: '<:BadgeEarlySupporter:1024918384682475570>',
+	EARLY_VERIFIED_BOT_DEVELOPER: '<:BadgeEarlyVerifiedBotDeveloper:1024918390273482793>',
+	HOUSE_BALANCE: '<:BadgeBalance:1024918349144145931>',
+	HOUSE_BRAVERY: '<:BadgeBravery:1024918354726752267>',
+	HOUSE_BRILLIANCE: '<:BadgeBrilliance:1024918361026613299>',
+	HYPESQUAD_EVENTS: '<:BadgeHypeSquadEvents:1024918396040650764>',
+	PARTNERED_SERVER_OWNER: '<:BadgePartner:1024918407335919646>',
+	TEAM_USER: ''
+};
