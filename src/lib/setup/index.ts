7b79747c745abe8db3b661e9bdc14b7ed0c3a294
Evo <85353424+EvolutionX-10@users.noreply.github.com>
2025-06-04 17:29:44 +0530

    refactor: remove unused scheduled tasks and clean up imports in package.json and index.ts

--- src/lib/setup/index.ts
+++ src/lib/setup/index.ts
@@ -1,5 +1,4 @@
 // import '@devtomio/plugin-botlist/register';
 import '@sapphire/plugin-editable-commands/register';
 import '@sapphire/plugin-logger/register';
-import '@sapphire/plugin-scheduled-tasks/register';
-console.clear();
+// console.clear();
